3f316072ac8b60a70c035e55f48c586318c9dcf7
Muhammad Afifudin <afif.x.udin@gmail.com>
2020-04-10 20:25:20 +0700

    Improve meta tag

--- views/main.php
+++ views/main.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="h-100">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="author" content="Muhammad Afifudin">
-	<meta name="theme-color" content="#1572e8">
-	<meta property="og:site_name" content="Presensi Kaizala">
+<?php include 'views/partials/meta.php'; ?>
 	<meta property="og:title" content="Presensi Kaizala · BPS Kabupaten Kayong Utara">
 	<meta property="og:url" content="<?=SITE.PATH?>">
 	<base href="<?=SITE?>/presensi-kaizala/">
