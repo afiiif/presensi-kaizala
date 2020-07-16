7d8daf716b0c9f7b1080f53c8311f863f82d8f2d
Muhammad Afifudin <afif.x.udin@gmail.com>
2020-07-16 21:05:15 +0700

    Add server/client time choice | Add GitHub button

--- assets.src/js/main/main.js
+++ assets.src/js/main/main.js
@@ -1,40 +1,30 @@
-$(()=>{
-
-	$(document).on('click', '[data-toggle="lightbox"]', function(e) {
-		e.preventDefault();
-		$(this).ekkoLightbox();
-	});
+$(() => {
 
 	const $$ = {
 		sectionInput: $('#section-input'),
 		sectionResult: $('#section-result'),
 		dropzone: $('#dropzone'),
-		display: $('[name="display_setting"]'),
 		tbl: $('#table'),
 	};
+
 	var $tbl,
 		data = [],
 		timezone = 7,
 		display = 0,
+		timeChoice = $('[name="time_setting"]:checked').val(),
 		employees,
 		sectionResult = false;
 
 	$$.dropzone
-	.on('click', function(e) { $('#input-files').trigger('click') })
-	.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) { e.preventDefault(); e.stopPropagation(); })
-	.on('dragover dragenter', function() { $$.dropzone.addClass('dragover') })
-	.on('dragleave dragend drop', function() { $$.dropzone.removeClass('dragover') })
-	.on('drop', function(e) { readFile(e.originalEvent.dataTransfer.files) });
+		.on('click', function (e) { $('#input-files').trigger('click') })
+		.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) { e.preventDefault(); e.stopPropagation(); })
+		.on('dragover dragenter', function () { $$.dropzone.addClass('dragover') })
+		.on('dragleave dragend drop', function () { $$.dropzone.removeClass('dragover') })
+		.on('drop', function (e) { readFile(e.originalEvent.dataTransfer.files) });
 
-	$$.display.change(function() {
-		display = Number($(this).val());
-		$$.tbl.toggleClass('td3-d-none td6-d-none', display === 0).toggleClass('td4-d-none td5-d-none', display === 1);
-		$tbl.draw();
-	});
+	$('#input-files').change(function (e) { readFile(e.target.files) });
 
-	$('#input-files').change(function(e) { readFile(e.target.files) });
-
-	$('#btn-reset').click(function() {
+	$('#btn-reset').click(function () {
 		$('.tooltip').tooltip('hide');
 		$$.sectionResult.hide(); sectionResult = false;
 		$$.sectionInput.fadeIn();
@@ -52,11 +42,11 @@ $(()=>{
 		const checkIfDone = () => {
 			if (n === i) {
 				if (nValid) {
-					utils.notif(nValid+' file berhasil diproses', 'success');
+					utils.notif(nValid + ' file berhasil diproses', 'success');
 					generateResult();
 				}
-				if (nInvalid) utils.notif((nValid ? nInvalid+' file' : 'File')+' tidak valid', 'danger');
-				if (nFailed) utils.notif(nFailed+' file gagal terbaca', 'warning');
+				if (nInvalid) utils.notif((nValid ? nInvalid + ' file' : 'File') + ' tidak valid', 'danger');
+				if (nFailed) utils.notif(nFailed + ' file gagal terbaca', 'warning');
 			}
 		}
 
@@ -69,7 +59,7 @@ $(()=>{
 					});
 					i++; nValid++; checkIfDone();
 				}
-				catch(err) {
+				catch (err) {
 					i++; nFailed++; checkIfDone();
 				}
 			}
@@ -77,20 +67,20 @@ $(()=>{
 		}
 
 		Array.from(files).forEach(file => {
-			let ext = file.name.substr(file.name.lastIndexOf('.')+1).toLowerCase();
+			let ext = file.name.substr(file.name.lastIndexOf('.') + 1).toLowerCase();
 			if (ext === 'csv') {
 				let reader = new FileReader();
 				reader.readAsText(file);
-				reader.onload = function(e) { readCsv(file.name, e.target.result.replace(/\t/g, ',')); }
-				reader.onerror = function() { i++; nFailed++; checkIfDone(); }
+				reader.onload = function (e) { readCsv(file.name, e.target.result.replace(/\t/g, ',')); }
+				reader.onerror = function () { i++; nFailed++; checkIfDone(); }
 			}
 			else if (ext === 'zip') {
-				JSZip.loadAsync(file).then(function(zip) {
+				JSZip.loadAsync(file).then(function (zip) {
 					let innerFiles = Object.keys(zip.files);
 					i -= innerFiles.length - 1;
 					innerFiles.forEach(function (filename) {
-						if (filename.substr(filename.lastIndexOf('.')+1).toLowerCase() === 'csv') {
-							zip.files[filename].async('string').then(function(text) {
+						if (filename.substr(filename.lastIndexOf('.') + 1).toLowerCase() === 'csv') {
+							zip.files[filename].async('string').then(function (text) {
 								readCsv(filename, text.replace(/\t/g, ',').replace(/\ufeff/g, ''));
 							});
 						}
@@ -106,7 +96,7 @@ $(()=>{
 		checkIfDone();
 	}
 
-	const generateResult = (resetDisplay=true) => {
+	const generateResult = (resetDisplay = true) => {
 
 		console.info(data);
 
@@ -119,13 +109,13 @@ $(()=>{
 		data.forEach(a => {
 			a.data.forEach(b => {
 
-				let m = moment(b['Server Receipt Timestamp (UTC)'].substr(0,19)).add(timezone, 'hours'),
+				let m = moment(b[timeChoice].substr(0, 19)).add(timezone, 'hours'),
 					day = m.format('dddd,D MMMM YYYY').split(','),
 					time = [m.format('HH:mm:ss'), Number(m.format('HHmmss'))];
 
 				if (!employees.includes(b['Responder Name'])) employees.push(b['Responder Name']);
 
-				let id_employee = employees.findIndex(c => c === b['Responder Name'])+1,
+				let id_employee = employees.findIndex(c => c === b['Responder Name']) + 1,
 					id_employee_d = id_employee + m.format('-YYYYMMDD'),
 					id_employee_t = id_employee + m.format('-YYYYMMDD-HHmmss');
 
@@ -145,30 +135,33 @@ $(()=>{
 		});
 
 		merged.forEach(a => {
-			let allTime = merged.filter(b => b.id_employee_d === a.id_employee_d).sort((a,b) => a.time[1]>b.time[1] ? 1 : -1),
+			let allTime = merged.filter(b => b.id_employee_d === a.id_employee_d).sort((a, b) => a.time[1] > b.time[1] ? 1 : -1),
 				n = allTime.length,
 				thisTimeIndex = allTime.findIndex(b => b.time[0] === a.time[0]);
 			if (thisTimeIndex === 0) {
 				if (n === 1) {
 					if (a.time[1] < 120000) {
 						a.range_time = [a.time[0], '-'];
-						a.range_coordinate = [a['Responder Location Latitude']+', '+a['Responder Location Longitude'], '-'];
+						a.range_coordinate = [a['Responder Location Latitude'] + ', ' + a['Responder Location Longitude'], '-'];
+						a.range_location = [a['Responder Location Location'], '-'];
 						a.range_note = [a.NotesQuestionTitle, '-'];
 						a.range_photo = [a.PhotoQuestionTitle, '-'];
 						a.dt_range_time = [a.dt_time, '<span class="text-gray">-</span>'];
 					}
 					else {
 						a.range_time = ['-', a.time[0]];
-						a.range_coordinate = ['-', a['Responder Location Latitude']+', '+a['Responder Location Longitude']];
+						a.range_coordinate = ['-', a['Responder Location Latitude'] + ', ' + a['Responder Location Longitude']];
+						a.range_location = ['-', a['Responder Location Location']];
 						a.range_note = ['-', a.NotesQuestionTitle];
 						a.range_photo = ['-', a.PhotoQuestionTitle];
 						a.dt_range_time = ['<span class="text-gray">-</span>', a.dt_time];
 					}
 				}
 				else {
-					let b = allTime[n-1];
+					let b = allTime[n - 1];
 					a.range_time = [a.time[0], b.time[0]];
-					a.range_coordinate = [a['Responder Location Latitude']+', '+a['Responder Location Longitude'], b['Responder Location Latitude']+', '+b['Responder Location Longitude']];
+					a.range_coordinate = [a['Responder Location Latitude'] + ', ' + a['Responder Location Longitude'], b['Responder Location Latitude'] + ', ' + b['Responder Location Longitude']];
+					a.range_location = [a['Responder Location Location'], b['Responder Location Location']];
 					a.range_note = [a.NotesQuestionTitle, b.NotesQuestionTitle];
 					a.range_photo = [a.PhotoQuestionTitle, b.PhotoQuestionTitle];
 					a.dt_range_time = [a.dt_time, b.dt_time];
@@ -179,7 +172,7 @@ $(()=>{
 		$tbl.clear().rows.add(merged).draw();
 		$$.tblFilter.html(
 			'<option value="">Semua Pegawai</option><option style="border-bottom: 1px solid #eee; font-size: 0; padding: 6px 0 0; margin-bottom: 6px;" disabled></option>'
-			+ employees.map((a,i) => [a, `<option value="${i+1}">${a}</option>`]).sort((a,b) => a[0]>b[0] ? 1 : -1).map(a => a[1]).join('')
+			+ employees.map((a, i) => [a, `<option value="${i + 1}">${a}</option>`]).sort((a, b) => a[0] > b[0] ? 1 : -1).map(a => a[1]).join('')
 		).trigger('change').selectpicker('refresh');
 
 		if (resetDisplay) {
@@ -190,8 +183,19 @@ $(()=>{
 		}
 	}
 
-	$('#select-timezone-2').change(function() {
-		dbg('Changed - '+sectionResult);
+	$('[name="display_setting"]').change(function () {
+		display = Number($(this).val());
+		$$.tbl.toggleClass('td3-d-none td6-d-none', display === 0).toggleClass('td4-d-none td5-d-none', display === 1);
+		$tbl.draw();
+	});
+
+	$('[name="time_setting"]').change(function () {
+		timeChoice = $(this).val();
+		generateResult(false);
+	});
+
+	$('#select-timezone-2').change(function () {
+		dbg('Changed - ' + sectionResult);
 		if (sectionResult) {
 			$('#select-timezone').selectpicker('val', $('#select-timezone-2').val());
 			generateResult(false);
@@ -204,43 +208,36 @@ $(()=>{
 			className: 'fit',
 			title: 'Hari, Tanggal',
 			data: 'dt_day',
-		},{
+		}, {
 			title: 'Nama',
 			data: null,
 			render: a => `<span class="d-none">[id=${a.id_employee}]</span>${a['Responder Name']}`,
-		},{
+		}, {
 			title: 'Waktu',
 			data: 'dt_time',
-		},{
+		}, {
 			title: 'Waktu Mulai',
 			data: null,
 			render: ({ dt_range_time }) => dt_range_time ? dt_range_time[0] : '',
-		},{
+		}, {
 			title: 'Waktu Berakhir',
 			data: null,
 			render: ({ dt_range_time }) => dt_range_time ? dt_range_time[1] : '',
-		},{
+		}, {
 			title: 'Catatan',
 			data: 'NotesQuestionTitle',
-		// },{
-		// 	className: 'fit pr-2',
-		// 	title: '',
-		// 	data: null,
-		// 	searchable: false,
-		// 	sortable: false,
-		// 	render: () => `<i class="fas fa-info-circle fa-fw py-1 cur-p fz-16 text-primary" title="Detail" onclick="utils.notif('Coming soon...', 'wait')"></i>`,
 		}],
 	});
 	$('#table_filter').html('<div class="d-inline-flex align-items-center"><div class="mr-2"><i class="fas fa-user"></i></div><div><select id="table-filter-employee" class="selectpicker" data-style="btn-light btn-sm"></select></div></div>');
-	$$.tblFilter = $('#table-filter-employee').change(function() { $tbl.draw() });
+	$$.tblFilter = $('#table-filter-employee').change(function () { $tbl.draw() });
 
-	$.fn.dataTable.ext.search.push(function(settings, data, dataIndex) {
-		return ( !$$.tblFilter.val() || data[1].includes(`[id=${$$.tblFilter.val()}]`) ) && ( display || data[3] );
+	$.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
+		return (!$$.tblFilter.val() || data[1].includes(`[id=${$$.tblFilter.val()}]`)) && (display || data[3]);
 	});
 
 	const exportExcel = sheets => {
 		const sanitizeName = name => {
-			if (name.length > 30) name = name.substr(0,27)+'...';
+			if (name.length > 30) name = name.substr(0, 27) + '...';
 			return name.replace(/\\|\/|\?|\*|\[|\]/g, '_');
 		}
 		let wb = XLSX.utils.book_new();
@@ -248,7 +245,7 @@ $(()=>{
 		XLSX.writeFile(wb, `Presensi Kaizala (${$('#select-timezone :selected').text()}) - ${moment.utc().add(timezone, 'hours').format('YYYYMMDD-HHmmss')}.xlsx`);
 	}
 
-	$('#btn-export').click(function() {
+	$('#btn-export').click(function () {
 
 		let rawdata = $tbl.rows().data().toArray(),
 			header,
@@ -256,19 +253,20 @@ $(()=>{
 			dataByEmployee = [];
 
 		if (display) {
-			header = ['Hari', 'Tanggal', 'Nama', 'Waktu', 'Koordinat', 'Catatan', 'Foto'];
+			header = ['Hari', 'Tanggal', 'Nama', 'Waktu', 'Koordinat', 'Lokasi', 'Catatan', 'Foto'];
 			data = rawdata.map(a => [
 				a.day[0],
 				a.day[1],
 				a['Responder Name'],
 				a.time[0],
-				a['Responder Location Latitude']+', '+a['Responder Location Longitude'],
+				a['Responder Location Latitude'] + ', ' + a['Responder Location Longitude'],
+				a['Responder Location Location'],
 				a.NotesQuestionTitle,
 				a.PhotoQuestionTitle,
 			]);
 		}
 		else {
-			header = ['Hari', 'Tanggal', 'Nama', 'Waktu Mulai', 'Waktu Berakhir', 'Koordinat Waktu Mulai', 'Koordinat Waktu Berakhir', 'Catatan Waktu Mulai', 'Catatan Waktu Berakhir', 'Foto Waktu Mulai', 'Foto Waktu Berakhir'];
+			header = ['Hari', 'Tanggal', 'Nama', 'Waktu Mulai', 'Waktu Berakhir', 'Koordinat Waktu Mulai', 'Koordinat Waktu Berakhir', 'Lokasi Waktu Mulai', 'Lokasi Waktu Berakhir', 'Catatan Waktu Mulai', 'Catatan Waktu Berakhir', 'Foto Waktu Mulai', 'Foto Waktu Berakhir'];
 			data = rawdata.filter(a => a.range_time).map(a => [
 				a.day[0],
 				a.day[1],
@@ -277,6 +275,8 @@ $(()=>{
 				a.range_time[1],
 				a.range_coordinate[0],
 				a.range_coordinate[1],
+				a.range_location[0],
+				a.range_location[1],
 				a.range_note[0],
 				a.range_note[1],
 				a.range_photo[0],
@@ -291,6 +291,11 @@ $(()=>{
 		let sheets = [{ name: 'Semua Pegawai', data: [header].concat(data) }].concat(dataByEmployee);
 		exportExcel(sheets);
 
+	});
+
+	$(document).on('click', '[data-toggle="lightbox"]', function (e) {
+		e.preventDefault();
+		$(this).ekkoLightbox();
 	});
 
 });
